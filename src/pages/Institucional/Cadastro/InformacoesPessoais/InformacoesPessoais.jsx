95b1c83a5af1604559864b49c2c229953dd8fbf0
clarasalomaofaria <clara.faria@sptech.school>
2023-10-08 18:03:19 -0300

    Criação do indicador de etapas no cadastro no componente CadastroPassos.jsx

--- src/pages/Institucional/Cadastro/InformacoesPessoais/InformacoesPessoais.jsx
+++ src/pages/Institucional/Cadastro/InformacoesPessoais/InformacoesPessoais.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CadastroPassos from "../../../../components/Institucional/CadastroPassos";
 
 function InformacoesPessoais() {
-    
+
     return (
         <>
+            <CadastroPassos corEndereco={"#AEBDBC"} corPlano={"#AEBDBC"} corCheckout={"#AEBDBC"}/>
         </>
     )
 }
