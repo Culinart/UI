bf696c541be65c89fc3c192557978b25aaf8bbb6
clarasalomaofaria <clara.faria@sptech.school>
2023-10-15 19:44:38 -0300

    Ajustes na chamada do componente CadastroPassos na página de Checkout

--- src/pages/Institucional/Cadastro/Checkout.jsx
+++ src/pages/Institucional/Cadastro/Checkout.jsx
@@ -8,7 +8,7 @@ function Checkout() {
         <>
             <div className="flex flex-col h-screen">
                 <Header />
-                <CadastroPassos corEndereco="#AEBDBC" corPlano="#AEBDBC" corCheckout="#AEBDBC" />
+                <CadastroPassos corEndereco="#2EC4B6" corPlano="#2EC4B6" corCheckout="#2EC4B6" />
                 <div className={`bg ${styles.bg}`}>
                     <div className={`card ${styles.card} flex`}>
                         <div className="flex flex-col">
