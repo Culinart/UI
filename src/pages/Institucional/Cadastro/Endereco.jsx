52bc6d5fa033734998ad56a519cfc8a3afd885b4
clarasalomaofaria <clara.faria@sptech.school>
2023-11-04 15:43:56 -0300

    Removendo a criação de session storage de usuario no cadastro de endereço

--- src/pages/Institucional/Cadastro/Endereco.jsx
+++ src/pages/Institucional/Cadastro/Endereco.jsx
@@ -37,10 +37,6 @@ function Endereco() {
     const [inputNumero, setInputNumero] = useState("");
     const [inputComplemento, setInputComplemento] = useState("");
 
-    const idUsuario = 1;
-    localStorage.setItem("idUsuario", idUsuario);
-
-
     const estados = [
         "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT",
         "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"
