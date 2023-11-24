356a2b2265c55fabe3419d9c9c0622a26c58e4eb
clarasalomaofaria <clara.faria@sptech.school>
2023-11-24 08:48:36 -0300

    Iniciando a integração do back end da página de pedidos e alterando a página para que seja mais simples

--- src/pages/Cliente/Pedidos.jsx
+++ src/pages/Cliente/Pedidos.jsx
@@ -1,30 +1,63 @@
-import React, { Children, useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderCliente from "../../components/Cliente/HeaderCliente/HeaderCliente";
-import sytle from "./Pedidos.module.css"
-
-import { IoIosHome } from "react-icons/io"
-import { TbCalendarSearch } from "react-icons/tb"
-
-import DatasCarrossel from "../../components/Cliente/Pedido/DatasCarrossel";
-import Carrossel from "../../components/Cliente/Pedido/Carrossel";
-import ModalPedido from "../../components/Cliente/Pedido/ModalPedido";
+import { FiEdit } from "react-icons/fi";
 import CardPedido from "../../components/Cliente/Pedido/CardPedido";
-import ModalCalendario from "../../components/Cliente/Pedido/ModalCalendario";
+import { useNavigate } from 'react-router-dom';
+import api from "../../api/api";
 
 
 function Pedidos() {
 
-    const [modalAberto, setModalAberto] = useState(false);
+    const navigate = useNavigate();
 
-    const handleAbrirModal = () => {
-        setModalAberto(true);
-    };
+    const [enderecoUsuario, setEnderecoUsuario] = useState({
+        usuario: {},
+        endereco: {},
+        isAtivo: null,
+    });
+    const [categorias, setCategorias] = useState([]);
 
-    const [modalCalendarioAberto, setModalCalendarioAberto] = useState(false);
 
-    const handleOpenCalendarModal = () => {
-        setModalCalendarioAberto(true);
-    };
+    const navigateToPage = (path) => {
+        navigate(path);
+    }
+
+    useEffect(() => {
+        buscarEnderecoAtivo();
+        buscarCategoriasUsuario();
+    }, []);
+
+    const buscarEnderecoAtivo = () => {
+        api.get(`/enderecos/usuarios/enderecoAtivo/${sessionStorage.getItem("idUsuario")}`, {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
+            }
+        })
+            .then((response) => {
+                setEnderecoUsuario(response.data);
+                console.log("ENDERECO: ", response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    const buscarCategoriasUsuario = () => {
+        api.get(`/planos/categorias/${sessionStorage.getItem("idUsuario")}`, {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
+            }
+        })
+            .then((response) => {
+                const categoriasNomes = response.data.map((planoCategoria) => planoCategoria.categoria.nome);
+                setCategorias(categoriasNomes);
+                console.log("CATEGORIAS: ", categoriasNomes);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
 
 
     return (
@@ -32,27 +65,28 @@ function Pedidos() {
             <HeaderCliente />
             <div className="flex justify-center w-full h-full mt-14">
                 <div className="flex flex-col items-center justify-center w-full h-full">
-                    <div className="items-center justify-center w-full flex text-[#045D53]">
-                        <h2 className="text-[1.3rem] mr-3 cursor-default">Data das próximas entregas</h2>
-                        <TbCalendarSearch onClick={handleOpenCalendarModal} className="cursor-pointer w-6 h-6" />
+                    <div className="items-center justify-between w-4/5 flex border-b border-gray-300">
+                        <h1 className="text-2xl text-[#045D53] mb-4">Pedido</h1>
                     </div>
-                    <DatasCarrossel />
-                    <div className="flex  justify-center items-center w-full bg-[#E1F0EF] mt-20 h-64">
+                    <div className="flex  justify-center items-center w-full bg-[#E1F0EF] mt-16 h-48">
                         <div className="flex justify-center flex-col w-7/12 h-auto">
-                            <p className="flex text-[#3F4747] text-[1.1rem] ml-5">Entrega
-                                <span className="ml-4 mr-4">-</span>
-                                <span className="flex items-center">
-                                    Rua Haddock Lobo, 595
-                                    <IoIosHome className="ml-2 mb-0.5 text-[#FF9F1C]" />
-                                </span></p>
-                            <ul className="text-[#3F4747] text-base text-[1rem] mt-8 ml-5">
-                                <li>Carnes, Peixes, Rápido e fácil</li>
-                                <li>3 receitas</li>
-                                <li>15 porções</li>
-                            </ul>
+                            <p className="flex-col text-[#3F4747] text-[1.1rem] ml-5">
+                                Entrega
+                                <p className="flex items-center">
+                                    {`${enderecoUsuario.endereco.logradouro}, ${enderecoUsuario.endereco.numero}`}
+                                    <FiEdit className="ml-2 mb-0.5 text-gray-600 cursor-pointer" onClick={() => navigateToPage('/cliente/perfil/endereco')} />
+                                </p>
+                                <span className="flex">
+                                    <p className=" mr-2">Data da próxima entrega: </p>
+                                    <p>00/00/0000</p>
+                                </span>
+                                {categorias.map((categoria, index) => (
+                                    <p key={index}>{categoria}</p>
+                                ))}
+                            </p>
                         </div>
                         <div className="flex justify-center items-center w-3/12 h-auto mt-4">
-                            <button onClick={handleAbrirModal} className="pl-3 pr-3 pb-2 pt-2 text-[1.1rem] text-[#FFFFFF] bg-[#DC7726] border-solid border-3 border-[#FF9F1C] rounded-md">
+                            <button className="pl-3 pr-3 pb-2 pt-2 text-[1.1rem] text-[#FFFFFF] bg-[#DC7726] border-solid border-3 border-[#FF9F1C] rounded-md">
                                 Editar Entrega
                             </button>
                         </div>
@@ -75,16 +109,6 @@ function Pedidos() {
                 </div>
             </div>
             <footer className="w-full h-20" />
-
-
-
-            {modalAberto ? (
-                <ModalPedido onClose={() => setModalAberto(false)} />
-            ) : null}
-
-            {modalCalendarioAberto ? (
-                <ModalCalendario onClose={() => setModalCalendarioAberto(false)} />
-            ) : null}
 
         </>
     )
