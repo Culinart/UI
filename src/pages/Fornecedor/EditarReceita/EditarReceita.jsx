3e2088e53a6a33b89279bd9a50583df1ce1cca67
rafael.Felips <rafael.msantos@sptech.school>
2023-11-24 00:26:36 -0300

    ultimos retoques meus nessa branch

--- src/pages/Fornecedor/EditarReceita/EditarReceita.jsx
+++ src/pages/Fornecedor/EditarReceita/EditarReceita.jsx
@@ -1,44 +1,94 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import api from "../../../api/api";
 import { useParams } from 'react-router-dom';
-import api from '../../../api/api';
 
 import HeaderFornecedor from "../../../components/Fornecedor/HeaderFornecedor/HeaderFornecedor";
-
-
 import style from "./EditarReceita.module.css"
+import trash from "../../../assets/Fornecedor/Receitas/trash.svg"
+import editar from "../../../assets/Fornecedor/Receitas/Edit.svg"
 
-function EditarReceita() {
+function AdicionarReceita() {
+
     const { id } = useParams();
 
-    // console.log(listaReceitas);
-    // console.log(listaReceitas);
+    const [receita, setReceita] = useState({
+        nome: '',
+        horas: '',
+        minutos: '',
+        rendimento: '',
+        descricao: '',
+        qtdAvaliacoes: '',
+        mediaAvaliacoes: '',
+        ingredientes: [{ quantidade: '', unidade: '', nome: '' }],
+        modoPreparo: [{ passo: '' }]
+    });
 
-    const [listaReceitas, setListaReceitas] = useState([])
-    const [receita, setReceita] = useState([])
 
-    useEffect(() => {
-        // buscarPreferencias();
-        buscarReceitasPedidos();
-    }, []);
-
-    const buscarReceitasPedidos = () => {
-        api.get('/receitas').then((response) => {
-            setListaReceitas(response.data);
-        }).catch((error) => {
+    axios.get(`${api}/receita/${id}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
             console.log(error);
         });
-    }
 
-    const encontrarReceita = () => {
-        for (var i = 0; i < listaReceitas; i++) {
-            alert('encontrou na encontrar receita')
+    const adicionarPasso = () => {
+        setReceita((prevState) => ({
+            ...prevState,
+            modoPreparo: [...prevState.modoPreparo, { passo: '' }],
+        }));
+    };
+
+    const adicionarIngrediente = () => {
+        setReceita((prevState) => ({
+            ...prevState,
+            ingredientes: [...prevState.ingredientes, { quantidade: '', unidade: '', nome: '' }],
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        api.post('/receitas', receita)
+            .then(response => {
+                console.log("Nova receita adicionada:", response.data);
+            })
+            .catch(error => {
+                console.error("Erro ao adicionar receita:", error);
+            });
+        console.log("esta é a receira", receita);
+    };
+
+
+    const removerIngrediente = (index) => {
+        if (index === 0 && receita.ingredientes.length === 1) {
+            return;
         }
-    }
+
+        setReceita((prevState) => {
+            const novosIngredientes = [...prevState.ingredientes];
+            novosIngredientes.splice(index, 1);
+            return { ...prevState, ingredientes: novosIngredientes };
+        });
+    };
+
+
+    const removerPasso = (index) => {
+        if (index === 0 && receita.modoPreparo.length === 1) {
+            return;
+        }
+
+        setReceita((prevState) => {
+            const novosPassos = [...prevState.modoPreparo];
+            novosPassos.splice(index, 1);
+            return { ...prevState, modoPreparo: novosPassos };
+        });
+    };
 
     return (
         <>
             <HeaderFornecedor />
-            <section className={style.body}>
+            <section onSubmit={handleSubmit} className={style.body}>
                 <div className={style.conteudo}>
                     <div className={style.topo}>
                         <h1 className={style.titulo_pagina}>Editar Receita {id}</h1>
@@ -52,11 +102,12 @@ function EditarReceita() {
                                 <input className={style.input_titulo} />
                             </label>
                             <div className={style.container_categoria}>
-                                <div className={style.icone} />
+                                <img src={editar} className={style.icone} alt="icone de lapis" />
                                 <span><b>Categoria: </b>Koreana</span>
                             </div>
                         </div>
                     </div>
+
                     <div className={style.container_medida_rendimento}>
                         <div className={style.container_rendimento_tempo}>
                             <div className={style.container_rendimento}>
@@ -67,7 +118,6 @@ function EditarReceita() {
                                     <span>porções</span>
                                 </div>
                             </div>
-
                             <div className={style.container_tempo}>
                                 <h1>Tempo de preparo</h1>
                                 <div className={style.tempo}>
@@ -78,45 +128,74 @@ function EditarReceita() {
                                 </div>
                             </div>
                         </div>
-
                         <div className={style.unidade_medida}>
                             <div className={style.titulos_medida}>
                                 <h1 className={style.titulo_quantidade}>Quantidade</h1>
                                 <h1 className={style.titulo_unidade}>Unidade</h1>
                                 <h1 className={style.titulo_ingrediente}>Ingrediente</h1>
                             </div>
-                            <div className={style.inputs_medida}>
-                                <input type="number" className={style.input_quantidade} />
-                                <select name="Unidade" className={style.input_unidade}>
-                                    <option value="valor1">Valor 1</option>
-                                </select>
-                                <input type="text" className={style.input_ingrediente} />
-                            </div>
-                            <button className={style.adicionar_ingrediente}>Adicionar Ingrediente</button>
+                            {receita.ingredientes.map((ingrediente, index) => (
+                                <div className={style.inputs_medida} key={index}>
+                                    <input
+                                        type="number"
+                                        defaultValue={ingrediente.quantidade}
+                                        className={style.input_quantidade}
+                                        onChange={(e) => handleInputChange(index, 'quantidade', e.target.value)}
+                                    />
+                                    <select name="select"
+                                        defaultValue={ingrediente.unidade}
+                                        className={style.input_unidade}
+                                        onChange={(e) => handleInputChange(index, 'unidade', e.target.value)}>
+                                        <option value="UNIDADE">Unidade</option>
+                                        <option value="LITRO">Litro</option>
+                                        <option value="KILO">Kilo</option>
+                                        <option value="GRAMA">Grama</option>
+                                        <option value="MILIGRAMA">Miligrama</option>
+                                        <option value="MILILITRO">Mililitro</option>
+                                        <option value="XICARA">Xicara</option>
+                                        <option value="SEM_UNIDADE">Sem Unidade</option>
+                                        <option value="COLHER_SOPA">Colher de sopa</option>
+                                        <option value="COLHER_CHA">Colher de chá</option>
+                                    </select>
+                                    <input
+                                        type="text"
+                                        defaultValue={ingrediente.nome}
+                                        className={style.input_ingrediente}
+                                        onChange={(e) => handleInputChange(index, 'nome', e.target.value)}
+                                    />
+                                    <img src={trash} className={style.icone} onClick={() => removerIngrediente(index)} alt="icone de lata de lixo" />
+                                </div>
+                            ))}
+                            <button
+                                className={style.adicionar_ingrediente}
+                                onClick={adicionarIngrediente}
+                            >
+                                Adicionar Ingrediente
+                            </button>
                         </div>
                     </div>
-
                     <div className={style.container_modo_preparo}>
                         <h1 className={style.titulo_preparo}>Modo de preparo</h1>
-                        <div className={style.passo}>
-                            <span>Passo 1</span>
-                            <textarea cols="30" rows="3" />
-                        </div>
-                        <button className={style.adicionar_passo}>Adicionar Passo</button>
-                    </div>
+                        {receita.modoPreparo.map((passo, index) => (
+                            <div key={index}>
+                                <div className={style.passo}>
+                                    <span>Passo {index + 1}</span>
+                                    <img src={trash} className={style.icone} onClick={() => removerPasso(index)} alt="icone de lata de lixo" />
+                                    <textarea cols="30" rows="3" defaultValue={passo.passo} onChange={(e) => handleInputChange(index, 'passo', e.target.value)} />
+                                </div>
 
+                            </div>
+                        ))}
+                        <button className={style.adicionar_ingrediente} onClick={adicionarPasso}> Adicionar passo</button>
+                    </div>
                     <div className={style.container_botoes}>
-                        <button>
-                            <a href="/fornecedor/receitas">
-                                Cancelar
-                            </a>
-                        </button>
-                        <button>Confirmar</button>
+                        <button>Cancelar</button>
+                        <button type="submit">Confirmar</button>
                     </div>
                 </div>
             </section>
         </>
-    );
+    )
 }
 
-export default EditarReceita;
+export default AdicionarReceita;
