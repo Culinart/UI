49efeba7d2512a7264300ee50b2a9b079e10f5f0
clarasalomaofaria <clara.faria@sptech.school>
2023-11-26 15:51:13 -0300

    Arrumando a navegação entre as páginas de pedido

--- src/pages/Cliente/Pedidos.jsx
+++ src/pages/Cliente/Pedidos.jsx
@@ -276,10 +276,12 @@ function Pedidos() {
     const handleDateNavigation = (direction) => {
         if (direction === "left" && selectedDateIndex > 0) {
             setSelectedDateIndex((prevIndex) => prevIndex - 1);
+            setDataPedidoAtual(datasPedidos[selectedDateIndex - 1].datasPedidos);
         } else if (direction === "right" && selectedDateIndex < datasPedidos.length - 1) {
             setSelectedDateIndex((prevIndex) => prevIndex + 1);
+            setDataPedidoAtual(datasPedidos[selectedDateIndex + 1].datasPedidos);
         }
-    };
+    };    
 
 
     return (
