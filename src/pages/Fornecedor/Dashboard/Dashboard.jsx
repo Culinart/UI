1e6b5e5875d66c9b693f294a7fde590680fd6fc0
clarasalomaofaria <clara.faria@sptech.school>
2023-12-03 12:00:12 -0300

    Arrumando e integrando as KPIs da dashboard ao backend

--- src/pages/Fornecedor/Dashboard/Dashboard.jsx
+++ src/pages/Fornecedor/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./Dashboard.module.css"
 
 import HeaderFornecedor from "../../../components/Fornecedor/HeaderFornecedor/HeaderFornecedor";
@@ -22,6 +22,86 @@ import Swal from "sweetalert2";
 function Dashboard() {
 
     const [categorias, setCategorias] = useState([]);
+    const [melhoresAvaliados, setMelhoresAvaliados] = useState([])
+    const [pioresAvaliados, setPioresAvaliados] = useState([])
+    const [melhoresCategorias, setMelhoresCategorias] = useState([])
+    const [pioresCategorias, setPioresCategorias] = useState([])
+    const [melhoresPreferencias, setMelhoresPreferencias] = useState([])
+    const [pioresPreferencias, setPioresPreferencias] = useState([])
+
+    useEffect(() => {
+        getAvaliacoes();
+        getCategorias();
+        getPreferencias();
+    }, []);
+
+    const getPreferencias = () => {
+        api.get('dashboard/preferencias', {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
+            }
+        }).then((response) => {
+            const sortedPreferencias = response.data.sort((a, b) => b.qtd_escolhas - a.qtd_escolhas);
+
+            const melhoresPref = sortedPreferencias.slice(0, 5);
+
+            const PioresPref = sortedPreferencias.slice(-5).reverse();
+
+            setMelhoresPreferencias(melhoresPref);
+            setPioresPreferencias(PioresPref);
+
+            console.log("PREFERENCIAS:", response.data);
+        }).catch((error) => {
+            console.log('Status do erro: ' + error.response.status);
+            console.log(error.message);
+        });
+    };
+
+    const getCategorias = () => {
+        api.get('dashboard/categorias', {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
+            }
+        }).then((response) => {
+            const sortedCategorias = response.data.sort((a, b) => b.qtd_escolhas - a.qtd_escolhas);
+
+            const melhoresCateg = sortedCategorias.slice(0, 5);
+
+            const pioresCateg = sortedCategorias.slice(-5).reverse();
+
+            setMelhoresCategorias(melhoresCateg);
+            setPioresCategorias(pioresCateg);
+
+            console.log("CATEGORIAS:", response.data);
+        }).catch((error) => {
+            console.log('Status do erro: ' + error.response.status);
+            console.log(error.message);
+        });
+    };
+
+
+    const getAvaliacoes = () => {
+        api.get('dashboard/receitas', {
+            headers: {
+                Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
+            }
+        }).then((response) => {
+            const sortedAvaliacoes = response.data.sort((a, b) => b.notaMedia - a.notaMedia);
+
+            const melhoresAvaliacoes = sortedAvaliacoes.slice(0, 5);
+
+            const pioresAvaliacoes = sortedAvaliacoes.slice(-5).reverse();
+
+            setMelhoresAvaliados(melhoresAvaliacoes);
+            setPioresAvaliados(pioresAvaliacoes);
+
+            console.log("AVALIACOES:", response.data);
+        }).catch((error) => {
+            console.log('Status do erro: ' + error.response.status);
+            console.log(error.message);
+        });
+    };
+
 
     const alertaErroDownload = () => {
         Swal.fire({
@@ -32,7 +112,7 @@ function Dashboard() {
         })
     }
 
-    // Exportaçãp com API
+    // Exportação com API
     const handleExportArquivoTxt = (e) => {
 
         e.preventDefault()
@@ -54,25 +134,23 @@ function Dashboard() {
         })
     }
 
-    const heandleCategorias = () => {
-        api.get('dashboard/categorias', {
-            headers: {
-                Authorization: `Bearer ${sessionStorage.getItem('authToken')}`
-            }
-        }).then((response) => {
-            console.log("entrei no .then do tratamento das categorias")
-            console.log(response.data)
-            setCategorias(response.data)
-        })
-    }
+    const handleCategorias = () => {
+        if (selectedOptionKpiEsquerda === "Categorias") {
+            setMelhoresCategorias(categorias.slice(0, 5));
+            setPioresCategorias(categorias.slice(-5).reverse());
+        } else if (selectedOptionKpiEsquerda === "Preferências") {
+            setMelhoresCategorias(melhoresPreferencias);
+            setPioresCategorias(pioresPreferencias);
+        }
+    };
 
-    const [selectedOptionKpiEsquerda, setSelectedOptionKpiEsquerda] = useState("");
+    const [selectedOptionKpiEsquerda, setSelectedOptionKpiEsquerda] = useState("Categorias");
 
     const opcoesKpiEsquerda = [
         "Categorias", "Preferências"
     ];
 
-    const [selectedOptionKpiDireita, setSelectedOptionKpiDireita] = useState("");
+    const [selectedOptionKpiDireita, setSelectedOptionKpiDireita] = useState("Melhor");
 
     const opcoesKpiDireita = [
         "Melhor", "Pior"
@@ -96,6 +174,30 @@ function Dashboard() {
 
     const [chartDataState, setChartDataState] = useState(chartData)
 
+    const renderMelhoresRank = () => {
+        if (selectedOptionKpiEsquerda === 'Categorias') {
+            return melhoresCategorias.map((categoria, index) => (
+                <MaisEscolhidaRank key={index} ranking={index + 1} categoria={categoria.categoria} quantidadeEscolhas={categoria.qtd_escolhas} />
+            ));
+        } else if (selectedOptionKpiEsquerda === 'Preferências') {
+            return melhoresPreferencias.map((preferencia, index) => (
+                <MaisEscolhidaRank key={index} ranking={index + 1} categoria={preferencia.preferencia} quantidadeEscolhas={preferencia.qtd_escolhas} />
+            ));
+        }
+    };
+
+    const renderPioresRank = () => {
+        if (selectedOptionKpiEsquerda === 'Categorias') {
+            return pioresCategorias.map((categoria, index) => (
+                <MenosEscolhidaRank key={index} ranking={index + 1} categoria={categoria.categoria} quantidadeEscolhas={categoria.qtd_escolhas} />
+            ));
+        } else if (selectedOptionKpiEsquerda === 'Preferências') {
+            return pioresPreferencias.map((preferencia, index) => (
+                <MenosEscolhidaRank key={index} ranking={index + 1} categoria={preferencia.preferencia} quantidadeEscolhas={preferencia.qtd_escolhas} />
+            ));
+        }
+    };
+
     return (
         <>
             <HeaderFornecedor />
@@ -111,21 +213,9 @@ function Dashboard() {
                                             value={selectedOptionKpiEsquerda}
                                             onChange={(e) => setSelectedOptionKpiEsquerda(e.target.value)}
                                         >
-                                            <option value="">--</option>
-                                            {opcoesKpiEsquerda.map((time, index) => (
-                                                <option key={index} value={time}>
-                                                    {time}
-                                                </option>
-                                            ))}
+                                            <option value="Categorias">Categorias</option>
+                                            <option value="Preferências">Preferências</option>
                                         </select>
-
-                                        <button onClick={handleExportArquivoTxt} className="flex justify-evenly items-center w-[23.5%] h-[1.7rem] mt-4 bg-[#C5C5C5] text-[#FFFFFF] text-[0.85rem] rounded-md hover:bg-[#2EC4B6] transition duration-200">
-                                            Baixar Avaliações
-                                            <MdOutlineFileDownload className="w-4 h-4" />
-                                        </button>
-
-                                        <button onClick={heandleCategorias}>a</button>
-
                                     </div>
                                 </div>
                                 <div className="flex w-full justify-center mt-10">
@@ -133,47 +223,54 @@ function Dashboard() {
                                         <div className={`${style.width_escolhidos} flex flex-col items-center`}>
                                             <h1 className="text-base font-medium">Mais Escolhidos</h1>
                                             <TituloRankingCategoriaMaisEscolhidos />
-                                            <MaisEscolhidaRank />
+                                            {renderMelhoresRank()}
                                         </div>
                                         <div className={`${style.width_escolhidos} flex flex-col items-center`}>
                                             <h1 className="text-base font-medium">Menos Escolhidos</h1>
                                             <TituloRankingCategoriaMenosEscolhidos />
-                                            <MenosEscolhidaRank />
+                                            {renderPioresRank()}
                                         </div>
                                     </div>
                                 </div>
                                 <div className="w-full h-5 mt-2" />
                             </div>
                             <div className={`${style.kpi_direita} bg-[#FFFFFF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] rounded-md`} >
-                                <select
-                                    className={`${style.combobox_horario_entrega} flex text-xs font-light pl-4 pr-4 ml-4 mt-6`}
-                                    value={selectedOptionKpiDireita}
-                                    onChange={(e) => setSelectedOptionKpiDireita(e.target.value)}
-                                >
-                                    <option value="">--</option>
-                                    {opcoesKpiDireita.map((time, index) => (
-                                        <option key={index} value={time}>
-                                            {time}
-                                        </option>
-                                    ))}
-                                </select>
+                                <span className="flex justify-between">
+                                    <select
+                                        className={`${style.combobox_horario_entrega} flex text-xs font-light pl-4 pr-4 ml-4 mt-6`}
+                                        value={selectedOptionKpiDireita}
+                                        onChange={(e) => setSelectedOptionKpiDireita(e.target.value)}
+                                    >
+                                        <option value="Melhor">Melhor</option>
+                                        <option value="Pior">Pior</option>
+                                    </select>
+
+                                    <button onClick={handleExportArquivoTxt} className="flex justify-evenly mr-8 items-center w-40 h-[1.7rem] mt-4 bg-[#C5C5C5] text-[#FFFFFF] text-[0.85rem] rounded-md hover:bg-[#2EC4B6] transition duration-200">
+                                        Baixar Avaliações
+                                        <MdOutlineFileDownload className="w-4 h-4" />
+                                    </button>
+                                </span>
                                 <div className="flex flex-col w-full items-center h-auto mt-10">
-                                    <TituloRankingMelhoresReceita />
-                                    <div className="flex flex-col w-full h-72 items-center overflow-hidden overflow-y-scroll mr-2.5 mt-2">
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                        <ReceitaMelhorAvaliada />
-                                    </div>
+                                    {selectedOptionKpiDireita === "Melhor" && (
+                                        <>
+                                            <TituloRankingMelhoresReceita />
+                                            <div className="flex flex-col w-full h-72 items-center overflow-hidden overflow-y-scroll mr-2.5 mt-2">
+                                                {melhoresAvaliados.map((receita, index) => (
+                                                    <ReceitaMelhorAvaliada key={index} index={index} receita={receita} />
+                                                ))}
+                                            </div>
+                                        </>
+                                    )}
+                                    {selectedOptionKpiDireita === "Pior" && (
+                                        <>
+                                            <TituloRankingPioresReceita />
+                                            <div className="flex flex-col w-full h-72 items-center overflow-hidden overflow-y-scroll mr-2.5 mt-2">
+                                                {pioresAvaliados.map((receita, index) => (
+                                                    <ReceitaPiorAvaliada key={index} index={index} receita={receita} />
+                                                ))}
+                                            </div>
+                                        </>
+                                    )}
                                 </div>
                                 <div className="w-full h-5" />
                             </div>
