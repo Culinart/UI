0bb59fd1ad3c64695e3d7c02a092760ce2fdeb38
gabsmvb <gabriel.boas@sptech.school>
2024-03-19 22:08:15 -0300

    fix ec2 aws

--- src/api/api.js
+++ src/api/api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 const IP = {
     gabriel: `http://10.18.34.91:8080`,
-    local: `http://localhost:8080`
+    local: `http://localhost:8080`,
+    ec2: `http://107.20.93.62:8080`
 }
 
 const api = axios.create({
-    baseURL: IP.local,
+    baseURL: IP.ec2,
     timeout: 15000,
     headers: {
         'Content-Type': 'application/json',
